7277b2cd35ecfd85f568a3fa8b8142ddf4ea88ad
priaw <priaw_raibon@hotmail.com>
2019-03-14 12:41:16 +0700

    addjobs

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const body = require('body-parser');  //user method post
 const mysql = require('mysql2');
 const app = express();
 
+app.use(body());
 //create connection
 /*const db = mysql.createConnection({
     host: 'localhost',
@@ -33,57 +35,88 @@ app.set('view engine','ejs');
     });
 });*/
 
+
 //path
 app.get('/',function(req, res){
     //var data = {name:"EIEI", age:23, job:"Pro"};
 
     res.render('home');
 });
+
 app.get('/listjob',function(req, res){
     //query data
     res.render('listjob');
 });
-app.post('/addlistjob',(req, res)=>{  
 
+//adddata jobs
+app.post('/addjobs',(req, res)=>{  
+    let data = {
+        name    : req.body.fname,
+        sorce   : req.body.Sorce,
+        des     : req.body.Des,
+        type    : req.body.type ,
+        price   : req.body.Price
+    }
+    //console.log(data);
+   /* let sql = "INSERT INTO listjobs
+    VALUES (data.name, data.sorce, data.des, data.type, data.pricr, data.ETC);";
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Add jobs')
+    });*/
+    //res.redirect('listjobs');  //redirect page
+
+    res.end("<h1>"+ data.name + "<br>" + data.sorce + "<br>" + data.des +"<br>" + data.type + "<br>" +  data.price + "</h1>");
+    
 });
+
 app.get('/payment',function(req, res){
     //query data
     res.render('payment');
 });
+
 app.get('/listpayment',function(req, res){
     //query data
     res.render('listpayment');
 });
+
 app.get('/salary',function(req, res){
     //query data
     res.render('salary');
 });
+
 app.get('/listcash_customer',function(req, res){
     //query data
     res.render('listcash_customer');
 });
+
 app.get('/detail_account',function(req, res){
     //query data
     res.render('detail_account');
 });
+
 app.get('/invoice',function(req, res){
     //query data
     res.render('invoice');
 });
+
 app.get('/bill',function(req, res){
     //query data
     res.render('bill');
 });
+
 app.get('/commission',function(req, res){
     res.render('commission');
 });
+
 app.get('/register',function(req, res){
     res.render('register');
 });
+
 app.get('/tracking',function(req, res){
     res.send('tracking');
 });
-
 
 //port 
 app.listen('3000', () => {
