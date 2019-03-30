227246ee66db329ca839c2e4984302237009740f
Worawat Pha <worawat.pha@gmail.com>
2019-03-30 16:17:13 +0700

    select option change by page

--- routes/listjobs.js
+++ routes/listjobs.js
@@ -6,20 +6,37 @@ moment.locale('th');
 
 router.get('/listjobs', function (req, res) {
     db.execute('SELECT * FROM `list-jobs`', function (err, result) {
-        
+
         if (err) {
             throw err;
         } else {
             listjob = {
                 print: result,
                 moment: moment,
-                
-                
+                page: 'all'
+
             };
-           
+
             res.render('listjobs', listjob);
         }
     });
+});
 
+router.get('/listjobs-customer', function (req, res) {
+    db.execute('SELECT * FROM `list-jobs` WHERE customer = 2', function (err, result) {
+
+        if (err) {
+            throw err;
+        } else {
+            listjob = {
+                print: result,
+                moment: moment,
+                page: 'customer'
+
+            };
+
+            res.render('listjobs', listjob);
+        }
+    });
 });
 module.exports = router;
