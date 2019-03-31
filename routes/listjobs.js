a3675eb8356d838a657b15b5dcd0e2da93312a71
Worawat Pha <worawat.pha@gmail.com>
2019-03-31 12:55:53 +0700

    try to send id prop from select options

--- routes/listjobs.js
+++ routes/listjobs.js
@@ -3,6 +3,23 @@ const router = express.Router();
 const moment = require('moment');
 const db = require('../models/database');
 moment.locale('th');
+const something = `listjobs`
+
+router.get('/listjobs-result', function (req, res) {
+    db.execute('SELECT * FROM `listjobs` JOIN customer ON listjobs.customer = customer.id JOIN dealer ON listjobs.dealer = dealer.id JOIN driver ON listjobs.driver = driver.id', function (err, result) {
+
+        if (err) {
+            throw err;
+        } else {
+            listjob = {
+                print: result,
+                moment: moment,
+                page: 'all'
+            };
+            res.render('listjobs', listjob);
+        }
+    });
+});
 
 router.get('/listjobs-all', function (req, res) {
     db.execute('SELECT * FROM `listjobs` JOIN customer ON listjobs.customer = customer.id JOIN dealer ON listjobs.dealer = dealer.id JOIN driver ON listjobs.driver = driver.id', function (err, result) {
