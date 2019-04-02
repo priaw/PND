8ed6c0461532c8255b5ffef17e03ee2e873914a5
Worawat Pha <worawat.pha@gmail.com>
2019-04-02 17:49:48 +0700

    search option

--- routes/listjobs.js
+++ routes/listjobs.js
@@ -4,55 +4,6 @@ const moment = require('moment');
 const db = require('../models/database');
 moment.locale('th');
 
-router.get('/listjobs-result', function (req, res) {
-    db.execute('SELECT * FROM `listjobs` JOIN customer ON listjobs.customer = customer.cust_id JOIN dealer ON listjobs.dealer = dealer.dealer_id JOIN driver ON listjobs.driver = driver.driver_id', function (err, result) {
-
-        if (err) {
-            throw err;
-        } else {
-            listjob = {
-                print: result,
-                moment: moment,
-                page: 'all'
-            };
-            res.render('listjobs', listjob);
-        }
-    });
-});
-
-router.get('/listjobs-all', function (req, res) {
-    db.execute('SELECT * FROM `listjobs` JOIN customer ON listjobs.customer = customer.cust_id JOIN dealer ON listjobs.dealer = dealer.dealer_id JOIN driver ON listjobs.driver = driver.driver_id ORDER BY listjobs.list_id', function (err, result) {
-
-        if (err) {
-            throw err;
-        } else {
-            listjob = {
-                print: result,
-                moment: moment,
-                page: 'all'
-            };
-            res.render('listjobs', listjob);
-        }
-    });
-});
-
-router.get('/listjobs-customer', function (req, res) {
-    db.execute('SELECT * FROM `listjobs` JOIN customer ON listjobs.customer = customer.cust_id JOIN dealer ON listjobs.dealer = dealer.dealer_id JOIN driver ON listjobs.driver = driver.driver_id ORDER BY customer.id', function (err, result) {
-
-        if (err) {
-            throw err;
-        } else {
-            listjob = {
-                print: result,
-                moment: moment,
-                page: 'customer'
-
-            };
-
-            res.render('listjobs', listjob);
-        }
-    });
-});
 router.get('/listjobs-date', function (req, res) {
     db.execute('SELECT * FROM `listjobs` JOIN customer ON listjobs.customer = customer.cust_id JOIN dealer ON listjobs.dealer = dealer.dealer_id JOIN driver ON listjobs.driver = driver.driver_id ORDER BY listjobs.date DESC', function (err, result) {
 
@@ -69,41 +20,7 @@ router.get('/listjobs-date', function (req, res) {
         }
     });
 });
-router.get('/listjobs-truck', function (req, res) {
-    db.execute('SELECT * FROM `listjobs` JOIN customer ON listjobs.customer = customer.cust_id JOIN dealer ON listjobs.dealer = dealer.dealer_id JOIN driver ON listjobs.driver = driver.driver_id ORDER BY driver.id', function (err, result) {
 
-        if (err) {
-            throw err;
-        } else {
-            listjob = {
-                print: result,
-                moment: moment,
-                page: 'truck'
-
-
-            };
-
-            res.render('listjobs', listjob);
-        }
-    });
-});
-router.get('/listjobs-driver', function (req, res) {
-    db.execute('SELECT * FROM `listjobs` JOIN customer ON listjobs.customer = customer.cust_id JOIN dealer ON listjobs.dealer = dealer.dealer_id JOIN driver ON listjobs.driver = driver.driver_id ORDER BY driver.driver_id', function (err, result) {
-
-        if (err) {
-            throw err;
-        } else {
-            listjob = {
-                print: result,
-                moment: moment,
-                page: 'driver',
-
-            };
-
-            res.render('listjobs', listjob);
-        }
-    });
-});
 
 ///////////////////
 router.get('/delete/:list_id', function (req, res) {
@@ -114,5 +31,93 @@ router.get('/delete/:list_id', function (req, res) {
     });
 });
 //////////////////
+
+
+// router.get('/listjobs-result', function (req, res) {
+//     db.execute('SELECT * FROM `listjobs` JOIN customer ON listjobs.customer = customer.cust_id JOIN dealer ON listjobs.dealer = dealer.dealer_id JOIN driver ON listjobs.driver = driver.driver_id', function (err, result) {
+
+//         if (err) {
+//             throw err;
+//         } else {
+//             listjob = {
+//                 print: result,
+//                 moment: moment,
+//                 page: 'all'
+//             };
+//             res.render('listjobs', listjob);
+//         }
+//     });
+// });
+
+// router.get('/listjobs-all', function (req, res) {
+//     db.execute('SELECT * FROM `listjobs` JOIN customer ON listjobs.customer = customer.cust_id JOIN dealer ON listjobs.dealer = dealer.dealer_id JOIN driver ON listjobs.driver = driver.driver_id ORDER BY listjobs.list_id', function (err, result) {
+
+//         if (err) {
+//             throw err;
+//         } else {
+//             listjob = {
+//                 print: result,
+//                 moment: moment,
+//                 page: 'all'
+//             };
+//             res.render('listjobs', listjob);
+//         }
+//     });
+// });
+
+// router.get('/listjobs-customer', function (req, res) {
+//     db.execute('SELECT * FROM `listjobs` JOIN customer ON listjobs.customer = customer.cust_id JOIN dealer ON listjobs.dealer = dealer.dealer_id JOIN driver ON listjobs.driver = driver.driver_id ORDER BY customer.id', function (err, result) {
+
+//         if (err) {
+//             throw err;
+//         } else {
+//             listjob = {
+//                 print: result,
+//                 moment: moment,
+//                 page: 'customer'
+
+//             };
+
+//             res.render('listjobs', listjob);
+//         }
+//     });
+// });
+
+// router.get('/listjobs-truck', function (req, res) {
+//     db.execute('SELECT * FROM `listjobs` JOIN customer ON listjobs.customer = customer.cust_id JOIN dealer ON listjobs.dealer = dealer.dealer_id JOIN driver ON listjobs.driver = driver.driver_id ORDER BY driver.id', function (err, result) {
+
+//         if (err) {
+//             throw err;
+//         } else {
+//             listjob = {
+//                 print: result,
+//                 moment: moment,
+//                 page: 'truck'
+
+
+//             };
+
+//             res.render('listjobs', listjob);
+//         }
+//     });
+// });
+// router.get('/listjobs-driver', function (req, res) {
+//     db.execute('SELECT * FROM `listjobs` JOIN customer ON listjobs.customer = customer.cust_id JOIN dealer ON listjobs.dealer = dealer.dealer_id JOIN driver ON listjobs.driver = driver.driver_id ORDER BY driver.driver_id', function (err, result) {
+
+//         if (err) {
+//             throw err;
+//         } else {
+//             listjob = {
+//                 print: result,
+//                 moment: moment,
+//                 page: 'driver',
+
+//             };
+
+//             res.render('listjobs', listjob);
+//         }
+//     });
+// });
+
 
 module.exports = router;
